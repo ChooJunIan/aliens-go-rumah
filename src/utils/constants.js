2436d7c2ddd83edd6386208155c84a0dcc136ef6
ChooJunIan <ianchoo2000@gmail.com>
2023-08-20 18:26:59 +0800

    added spawning enemies

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,15 @@
 export const skyAndGroundWidth = 5000;
+export const gameWidth = 800;
+export const createInterval = 500;
+export const maxFlyingObjects = 20;
+export const flyingObjectsStarterYAxis = -1000;
+export const flyingObjectsStarterXAxis = [
+    -600,
+    -450,
+    -300, 
+    -150, 
+    150, 
+    300,
+    450,
+    600, 
+];
